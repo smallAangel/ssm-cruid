92e32dfe783a17ff014374c6a9dc605f105bf694
chenxin <749807920@qq.com>
2020-10-26 01:53:42 +0800

    first!

--- src/main/java/com/cx/bean/User.java
+++ src/main/java/com/cx/bean/User.java
@@ -1,25 +1,11 @@
 package com.cx.bean;
 
-import java.io.Serializable;
+public class User {
+    private Integer id;
 
-/**
- * @author cx
- * @create 2020-10-24 22:42
- */
-public class User implements Serializable {
-    private static final long serialVersionUID = -684979447710L;
-    Integer id;
-    String name;
-    String pwd;
+    private String name;
 
-    public User() {
-    }
-
-    public User(Integer id, String name, String pwd) {
-        this.id = id;
-        this.name = name;
-        this.pwd = pwd;
-    }
+    private String pwd;
 
     public Integer getId() {
         return id;
@@ -34,23 +20,14 @@ public class User implements Serializable {
     }
 
     public void setName(String name) {
-        this.name = name;
+        this.name = name == null ? null : name.trim();
     }
 
     public String getPwd() {
         return pwd;
     }
 
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", pwd='" + pwd + '\'' +
-                '}';
-    }
-
     public void setPwd(String pwd) {
-        this.pwd = pwd;
+        this.pwd = pwd == null ? null : pwd.trim();
     }
 }
